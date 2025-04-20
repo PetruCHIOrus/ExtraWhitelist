e47c56ed2a94e22438c8d4dfd300698047819e64
PetruCHIOrus <zhidkinpeter@yandex.ru>
2025-04-20 14:34:50 +0300

    Initial test release

---
+++ src/ExtraWhiteList/pluginConfig.java
@@ -0,0 +1,36 @@
+package ExtraWhiteList;
+
+import arc.files.Fi;
+import arc.util.Log;
+import arc.util.serialization.Json;
+import mindustry.Vars;
+
+public class pluginConfig {
+    public int listMode = 0;
+    private static final String configFileName = "extrawhitelist.json";
+    private static final Fi configDir = Vars.dataDirectory.child("config");
+    private static final Fi configFile = configDir.child(configFileName);
+    public String PlayerlistDir = "config/playerlist.txt";
+    public void load() {
+        if(configFile.exists()){
+            try {
+                Json json = new Json();
+                pluginConfig loaded = json.fromJson(pluginConfig.class, configFile.readString());
+                this.listMode = loaded.listMode;
+                this.PlayerlistDir = loaded.PlayerlistDir;
+            } catch (Exception e){
+                Log.err("Can't load extrawhitelist.json: " + e);
+            }
+        } else {
+            save();
+        }
+    }
+    public void save() {
+        try {
+            Json json   = new Json();
+            configFile.writeString(json.prettyPrint(this));
+        } catch (Exception e) {
+            Log.err("Can't save extrawhitelist.json: " + e);
+        }
+    }
+}
